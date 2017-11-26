10050bbf7aa8cd1e10061ac676cffdce53c074cf
grzegomill <grzegomill@gmail.com>
2017-11-26 09:12:07 +0100

    comment added

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -2,7 +2,7 @@ package com.kodilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//first spring appl
 @SpringBootApplication
 public class KodillaCourseApplication {
 
